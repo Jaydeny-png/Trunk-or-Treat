8ebb359e11e21594f6967201e177dc5b8f4d4001
Jaydeny-png <144464858+Jaydeny-png@users.noreply.github.com>
2024-10-11 09:28:55 -0400

    Lots of Work need more time

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    // Attach the click event handler using .on() method
     $('.more-info-btn').on('click', function(){
-        // Find the corresponding event description and toggle it
-        $(this).closest('.event').find('.event-description').toggle();
+        $(this).next('.event-description').toggle();
     });
 });
+
+
+
+
+
+
