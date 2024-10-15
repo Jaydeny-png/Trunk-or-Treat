d7edf1776c963e78f307e944a1f0d203c59ac941
Jaydeny-png <144464858+Jaydeny-png@users.noreply.github.com>
2024-10-15 10:24:02 -0400

    libsun

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
-    $('.more-info-btn').on('click', function(){
-        $(this).next('.event-description').toggle();
-    });
+$('#openModal').click(function() {
+    $('#ex1').modal();
 });
 
 
+$(document).ready(function(){
+    $('.trunk-image').mouseenter(function() {
+      $(this).next('.trunk-description').fadeIn();
+    }).mouseleave(function() {
+      $(this).next('.trunk-description').fadeOut();
+    });
+  });
+ 
 
-
-
-
+  $(document).ready(function() {
+    $('.event-heading').on('click', function() {
+      $(this).next('.event-details').slideToggle(); // Toggle the details
+    });
+  });
